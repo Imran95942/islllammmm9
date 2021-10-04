d2e278ac2bf20f01d00867711db9e161c113820c
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-10-04 12:18:31 +0300

    Update musicplay.py

--- AnkiVector/modules/musicplay.py
+++ AnkiVector/modules/musicplay.py
@@ -51,20 +51,20 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 1) Get your channel ID.
 2) Create a group with tittle: Channel Music: your_channel_id
 3) Add bot as Channel admin with full perms
-4) Add @AnkivectorMusicbot to the channel as an admin.
+4) Add @islam95 to the channel as an admin.
 5) Simply send commands in your group.
  
 * ⚫️More tools⚫️ *
 
 • /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-• /userbotjoin: Invite @AnkivectorHelper Userbot to your chat 
+• /userbotjoin: Invite @islam95 Userbot to your chat 
  
 * 🔷Assisten🔷 *
 
-@AnkivectorHelper
+@islam95
 
 * 🔸Bot🔸 *
 
-@TheAnkiVectorBot
+@islam95
 """
 __mod_name__ = "Music Play"
